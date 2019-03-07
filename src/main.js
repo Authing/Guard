198d366db2b41166df9ea28c394dab0e3697ab54
ivy <597055914@qq.com>
2019-03-07 14:38:02 +0800

    Make scope

--- src/main.js
+++ src/main.js
@@ -155,7 +155,7 @@ AuthingGuard.prototype = {
     const appId = this.querySearch('app_id') || '';
     const redirectURI = this.querySearch('redirect_uri') || '';
     const responseType = this.querySearch('response_type') || '';
-    const scope = this.querySearch('scope') || '';
+    const scope = this.querySearch('scope') || Math.ceil(Math.random() * Math.pow(10, 6));
     this.userAuthorizeURL = `${ssoHost}/login/authorize/confirm?app_id=${appId}&state=${state}&redirect_uri=${redirectURI}&response_type=${responseType}&scope=${scope}`;
     this.sysAuthorizeURL = `${ssoHost}/authorize?app_id=${appId}&state=${state}&redirect_uri=${redirectURI}&response_type=${responseType}&scope=${scope}&authorization_header=${localStorage.getItem(
       "_authing_token"
