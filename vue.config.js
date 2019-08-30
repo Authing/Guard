390f0440dd8b3717246739206305ab93a52d86bd
Mark Yeh <yelexin@hotmail.com>
2019-08-30 09:27:03 +0800

    feat: 增加在线调试 Guard 配置

--- vue.config.js
+++ vue.config.js
@@ -10,10 +10,12 @@ module.exports = {
   devServer: {
     proxy: {
       '^/authorize': {
+        // target: 'https://sso.authing.cn/sso',
         target: 'http://localhost:5556/sso',
         changeOrigin: true
       },
       '^/oauth': {
+        // target: 'https://sso.authing.cn/',
         target: 'http://localhost:5556/',
         changeOrigin: true
       }
