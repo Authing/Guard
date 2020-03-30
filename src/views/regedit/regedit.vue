18cd0c5044bca37ed1a3dbe4909bcf707dec021d
hanjvlitstone <15033537505@163.com>
2020-03-30 12:26:36 +0800

    regedit选择学校改成和教师认证一样

--- src/views/regedit/regedit.vue
+++ src/views/regedit/regedit.vue
@@ -8,7 +8,7 @@
       </span>
     </el-dialog>
     <div class="regBox">
-      <el-form :model="currentUser" label-width="150px" :rules="rules" ref="ruleForm">
+      <el-form :model="currentUser" label-width="150px" :rules="rules" ref="currentUser">
         <el-form-item label="昵称">
           <el-input v-model="currentUser.name" class="regInput"></el-input>
         </el-form-item>
@@ -19,25 +19,13 @@
         <el-form-item label="出生日期">
           <el-date-picker v-model="currentUser.birthDate" type="date" placeholder="请选择出生日期"></el-date-picker>
         </el-form-item>
-        <el-form-item label="所在省份" required prop="province">
-          <el-select v-model="currentUser.province" placeholder="请选择">
-            <el-option
-              v-for="item in proOptions"
-              :key="item.id"
-              :label="item.description"
-              :value="item.description"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="所在学校" required prop="school">
-          <el-select v-model="currentUser.school" placeholder="请选择" filterable>
-            <el-option
-              v-for="item in schOptions"
-              :key="item.nodeid"
-              :label="item.text"
-              :value="item.text"
-            ></el-option>
-          </el-select>
+        <el-form-item label="学校" required style="margin-bottom:9px">
+          <span
+            class="schoolSpan"
+            @click="alertChooseProvince"
+          >{{schoolTextShow?province1+"-"+school1:"点击选择学校"}}</span>
+          <span class="f_12 black3 padl15">请填写所在学校，认证后可以修改</span>
+          <span class="schoolTips" v-show="showSchoolTips">学校不能为空</span>
         </el-form-item>
         <el-form-item label="用户职业" required prop="job">
           <el-radio-group v-model="currentUser.job">
@@ -131,10 +119,50 @@
         <span>高等教育出版社用户中心</span>
       </a>
     </div>
+    <div class="modelProvince" v-show="provinceChooseShow">
+      <span style="font-weight:bold;
+    margin: 8px 8px 8px;
+    display: inline-block;">选择:</span>
+      <hr />
+      <div class="provinceBox">
+        <span
+          class="proOptions"
+          v-for="item in provinceList"
+          :key="item.nodeid"
+          @click="chooseProvince(item)"
+        >{{item.text}}</span>
+      </div>
+    </div>
+    <div class="modelSchool" v-show="schoolChooseShow">
+      <span style="font-weight:bold;
+    margin: 8px 8px 8px;
+    display: inline-block;">
+        {{currentProvince.text}}
+        <span
+          style="font-weight: normal;
+    margin-left: 806px;cursor:pointer
+"
+          @click="closeChoose"
+        >X</span>
+      </span>
+      <hr />
+      <div class="schoolBox" v-if="schoolList">
+        <span
+          class="schOptions"
+          v-for="item in schoolList"
+          :key="item.nodeid"
+          @click="chooseSchool(item)"
+        >{{item.text}}</span>
+      </div>
+      <div class="schoolNull" v-else>
+        <span>此城市下没有找到大学哦~</span>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import { mapGetters, mapActions } from "vuex";
+import schoolJson from "./../../../public/school.json";
 export default {
   data() {
     let validateSure = (rule, value, callback) => {
@@ -156,9 +184,12 @@ export default {
         checked: true,
         avatar: ""
       },
+      provinceChooseShow: false,
+      schoolChooseShow: false,
+      schoolList: [],
+      schoolTextShow: false,
+      showSchoolTips: false,
       dialogShow: false,
-      proOptions: null,
-      schOptions: [],
       majorOptions: [],
       $authing: null,
       opts: {},
@@ -171,9 +202,6 @@ export default {
       removeDom: false,
       showImg: false,
       rules: {
-        province: [
-          { required: true, message: "请选择省份", trigger: "change" }
-        ],
         school: [
           {
             required: true,
@@ -186,14 +214,18 @@ export default {
       },
       showInterTip: false,
       userId: "",
-      currentProvince: "",
-      userInfo: ""
+      currentProvince: { text: "北京" },
+      userInfo: "",
+      testJson: [],
+      provinceList: [],
+      province1: "",
+      school1: ""
     };
   },
   created() {
     this.$authing = this.$root.$data.$authing;
     this.opts = this.$root.$data.$authing.opts;
-
+    this.testJson = schoolJson;
     // 将协议的 query 参数存入 vuex
     this.saveProtocol({
       protocol: this.opts.protocol || this.$route.query.protocol,
@@ -254,22 +286,13 @@ export default {
       window.validAuth.clientInfo = userPoolSettings;
       this.$authing.pub("authing-load", validAuth);
 
-      fetch("https://node2d-public.hep.com.cn/province.json").then(res => {
-        res.json().then(resp => {
-          this.proOptions = resp.province;
-        });
-      });
       fetch("https://node2d-public.hep.com.cn/zy.json").then(res => {
         res.json().then(resp => {
           this.majorOptions = resp;
         });
       });
-      this.schOptions = [];
-      fetch("https://node2d-public.hep.com.cn/school.json").then(res => {
-        res.json().then(resp => {
-          this.schOptions = resp;
-        });
-      });
+      this.handleSchool();
+
       window.validAuth.metadata(this.userId).then(res => {
         if (res) {
           for (let val of res.list) {
@@ -289,6 +312,11 @@ export default {
                 if (!this.currentUser.nickname) {
                   this.currentUser.nickname = this.userInfo.nickname;
                 }
+                if (this.currentUser.school) {
+                  this.province1 = this.currentUser.province;
+                  this.school1 = this.currentUser.school;
+                  this.schoolTextShow = true;
+                }
               }
             }
           }
@@ -300,6 +328,42 @@ export default {
     sessionStorage.removeItem("jump2Profile");
   },
   methods: {
+    chooseProvince(val) {
+      this.showSchoolTips = false;
+      this.currentProvince = val;
+      this.provinceChooseShow = false;
+      this.schoolChooseShow = true;
+      //从jsonTest里面 找到 nodeid==val.node的,this.schoolList = nodes
+      for (let value of this.testJson) {
+        if (value.nodeid === val.nodeid) {
+          this.schoolList = value.nodes;
+        }
+      }
+    },
+    chooseSchool(val) {
+      this.schoolTextShow = true;
+      this.school1 = val.text;
+      this.province1 = this.currentProvince.text;
+      this.currentUser.province = this.currentProvince.text;
+      this.schoolChooseShow = false;
+      this.currentUser.school = val.text;
+    },
+    alertChooseProvince() {
+      this.provinceChooseShow = true;
+    },
+    handleSchool() {
+      for (let val of this.testJson) {
+        this.provinceList.push(val);
+      }
+    },
+    closeChoose() {
+      this.schoolChooseShow = false;
+      if (this.currentUser.school) {
+        this.schoolTextShow = true;
+      } else {
+        this.schoolTextShow = false;
+      }
+    },
     handleLogin() {
       //跳转到登录页面
       sessionStorage.setItem("jumpRegedit", true);
@@ -328,26 +392,19 @@ export default {
       }
     },
     onSubmit() {
-      this.$refs["ruleForm"].validate(valid => {
+      this.$refs["currentUser"].validate(valid => {
         if (valid) {
           if (
             this.currentUser.intreMajor1 ||
             this.currentUser.intreMajor2 ||
             this.currentUser.intreMajor3
           ) {
-            window.validAuth
-              .setMetadata({
-                _id: this.userId,
-                key: "currentUser",
-                value: JSON.stringify(this.currentUser)
-              })
-              .then(res => {
-                if (res) {
-                  $message.success({ message: "您已成功完善信息" });
-                }
-                this.backPage();
-              });
-            ////////这里提交表单
+            if (this.currentUser.school) {
+              this.handleSubmit();
+            } else {
+              this.showSchoolTips = true;
+              return false;
+            }
           } else {
             this.showInterTip = true;
             return false;
@@ -357,6 +414,25 @@ export default {
           return false;
         }
       });
+    },
+    handleSubmit() {
+      window.validAuth
+        .setMetadata({
+          _id: this.userId,
+          key: "currentUser",
+          value: JSON.stringify(this.currentUser)
+        })
+        .then(res => {
+          //在这里执行update  更新昵称
+          window.validAuth.update({
+            _id: this.userId,
+            nickname: this.currentUser.name
+          });
+          if (res) {
+            $message.success({ message: "您已成功完善信息" });
+          }
+          this.backPage();
+        });
     },
     ...mapActions("visibility", [
       "gotoWxQRCodeScanning",
@@ -472,5 +548,87 @@ export default {
   background: rgba(255, 255, 255, 0.1);
   -webkit-box-shadow: 2px -2px 5px #eaeaea;
   box-shadow: 2px -2px 5px #eaeaea;
+}
+.schoolTips {
+  color: red;
+  font-size: 12px;
+  display: block;
+  line-height: 20px;
+}
+.modelProvince {
+  width: 50%;
+  background: #f5f7faf0;
+  border: 1px solid #cccccc;
+  height: 300px;
+  position: absolute;
+  left: 325px;
+  top: 240px;
+  border-radius: 5px;
+}
+.provinceBox {
+  width: 100%;
+  height: 256px;
+  overflow: auto;
+}
+.schoolBox {
+  width: 100%;
+  height: 332px;
+  overflow: auto;
+}
+.schoolNull {
+  text-align: center;
+  display: block;
+  width: 100%;
+  height: 350px;
+  overflow: auto;
+}
+.modelSchool {
+  width: 70%;
+  background: #f5f7faf0;
+  border: 1px solid #cccccc;
+  height: 393px;
+  position: absolute;
+  left: 200px;
+  top: 240px;
+  border-radius: 5px;
+}
+.schoolSpan {
+  display: inline-block;
+  width: 220px;
+  border: 1px solid #cccccc7d;
+  border-radius: 5px;
+  line-height: 38px;
+  text-align: center;
+  color: #606266;
+  cursor: pointer;
+}
+.proOptions {
+  cursor: pointer;
+  width: 146px;
+  margin-right: 6px;
+  margin-bottom: 10px;
+  display: inline-block;
+  text-align: center;
+}
+.schOptions {
+  cursor: pointer;
+  width: 272px;
+  margin-right: 6px;
+  margin-bottom: 10px;
+  display: inline-block;
+  text-align: center;
+}
+.proOptions:hover,
+.schOptions:hover {
+  color: rgba(11, 125, 149, 1);
+}
+.f_12 {
+  font-size: 12px;
+}
+.black3 {
+  color: #999;
+}
+.padl15 {
+  padding-left: 15px;
 }
 </style>
