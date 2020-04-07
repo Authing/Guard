97788ba6f552118c925629d5ec77936f438ba3ad
Holegots <>
2020-04-07 12:01:48 +0800

    feat: add https suport on local develop

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,30 @@
+let host = process.env.ServerHost||'http://localhost:5510'
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
     output: {
-      libraryExport: 'default'
+      libraryExport: "default"
     }
   },
   css: { extract: false },
-  publicPath: process.env.NODE_ENV === 'production' ? '/login' : '/',
+  publicPath: process.env.NODE_ENV === "production" ? "/login" : "/",
   devServer: {
+    https: true,
     disableHostCheck: true,
+    port: 443,
     proxy: {
-      '^/authorize': {
-        // target: 'https://sso.authing.cn/sso',
-        target: 'http://localhost:5510/sso',
+      "^/authorize": {
+        target: `${host}/sso`,
         changeOrigin: true
       },
-      '^/oauth': {
-        //  target: 'https://sso.authing.cn/',
-        target: 'http://localhost:5510/',
+      "^/oauth": {
+        target: `${host}`,
+        changeOrigin: true
+      },
+      "^/cas/session": {
+        target: `${host}`,
         changeOrigin: true
       }
     }
   }
-}
+};
