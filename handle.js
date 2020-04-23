84e0fb8eb3786a32c361b2435dd2cb7f75ac23c3
Holegots <>
2020-04-23 16:25:18 +0800

    update: update handle

--- handle.js
+++ handle.js
@@ -1,25 +1,4 @@
 const fs = require('fs');
 let txt = fs.readFileSync('node_modules/@vue/cli-service/lib/commands/build/resolveLibConfig.js').toString();
-let count = 1;
-let start = 0,
-  end = 0;
-for (let i = 0; i < txt.length; i++) {
-  if (txt[i] === '\n') {
-    count++;
-  }
-  if (count === 55) {
-    start = i + 1;
-  }
-  if (count === 66) {
-    end = i - 1;
-  }
-}
-let left = txt.slice(0, start);
-let content = txt.slice(start, end);
-let right = txt.slice(end);
-content = content
-  .split('\n')
-  .map(item => '//' + item)
-  .join('\n');
-txt = left + content + right;
+txt = txt.replace(/\/\/adjust css output name ([\s\S]*?)\n\n/g ,"")
 fs.writeFileSync('node_modules/@vue/cli-service/lib/commands/build/resolveLibConfig.js', txt);
