32af28e83f1e90892e44f75a233b766cd2cf9d64
Holegots <>
2020-04-23 16:32:10 +0800

    feat: update ci and handle

--- handle.js
+++ handle.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 let txt = fs.readFileSync('node_modules/@vue/cli-service/lib/commands/build/resolveLibConfig.js').toString();
-txt = txt.replace(/\/\/adjust css output name ([\s\S]*?)\n\n/g ,"")
+txt = txt.replace(/\/\/ externalize Vue([\s\S]*?)\n\n/g ,"")
 fs.writeFileSync('node_modules/@vue/cli-service/lib/commands/build/resolveLibConfig.js', txt);
