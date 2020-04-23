9499986dda1788416ffafb82d92bb02e4a040b12
Holegots <>
2020-04-23 15:05:17 +0800

    fix: 修复推送错误

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+const fs = require('fs')
 let host = process.env.ServerHost||'http://localhost:5510'
 module.exports = {
   lintOnSave: false,
@@ -10,6 +12,8 @@ module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/login" : "/",
   devServer: {
     https: true,
+    cert: fs.readFileSync(path.join(__dirname,'ssl/fullchain1.pem')),
+    key: fs.readFileSync(path.join(__dirname,'ssl/privkey1.pem')),
     disableHostCheck: true,
     port: 443,
     proxy: {
