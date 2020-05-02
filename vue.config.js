8d82ba84839976dfec3097ebd1e17a6d1a596ddf
Holegots <>
2020-05-02 20:30:57 +0800

    fix: 增加cdn 上传功能

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,33 @@
 const fs = require("fs");
+const QiniuWebpackPlugin = require('better-qiniu-webpack-plugin')
+const config = require('./config')
+const productionConf = config.production
+const qiniuConf = productionConf.qiniu
+let plugins = [];
+if (config && config.useCdn) {
+  plugins.push(
+    // 打包后的文件都上传至七牛云
+    new QiniuWebpackPlugin({
+      accessKey: qiniuConf.accessKey,
+      secretKey: qiniuConf.secretKey,
+      bucket: qiniuConf.bucket,
+      bucketDomain: qiniuConf.bucketDomain,
+      matchFiles: ['!*.html', '!*.map'],
+      uploadPath: productionConf.distPath,
+      batch: 10,
+      // 是否增量构建
+      deltaUpdate: true,
+    })
+  )
+}
+
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
     output: {
       libraryExport: "default"
-    }
+    },
+    plugins,
   },
   css: { extract: false },
   publicPath: process.env.NODE_ENV === "production" ? "/login" : "/",
