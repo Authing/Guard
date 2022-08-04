40e875f75fb8f24315ea67ca8a760dc84658cc51
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2022-08-04 19:33:22 +0800

    feat: guard-angular and other a little modify

---
+++ packages/guard-angular/src/guard.module.ts
@@ -0,0 +1,29 @@
+import { NgModule, ModuleWithProviders } from '@angular/core'
+
+import { GuardService } from './guard.service'
+import { GuardClientService, GuardClientFactory } from './guard.client'
+import { GuardConfigService, GuardClientConfig } from './guard.config'
+
+import { GuardOptions } from '@authing/guard'
+
+
+@NgModule()
+export class GuardModule {
+  static forRoot (config: GuardOptions): ModuleWithProviders<GuardModule> {
+    return {
+      ngModule: GuardModule,
+      providers: [
+        GuardService,
+        {
+          provide: GuardConfigService,
+          useValue: config
+        },
+        {
+          provide: GuardClientService,
+          useFactory: GuardClientFactory.createClient,
+          deps: [GuardClientConfig]
+        },
+      ]
+    }
+  }
+}
---
+++ packages/guard-angular/src/index.ts
@@ -0,0 +1,9 @@
+import { GuardModule } from './guard.module'
+import { GuardService } from './guard.service'
+
+import '@authing/guard/dist/esm/guard.min.css'
+
+export {
+  GuardModule,
+  GuardService
+}
