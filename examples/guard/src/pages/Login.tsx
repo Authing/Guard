f504a5e64708cbd613f8761a7bd2326356a79052
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2022-08-18 10:50:20 +0800

    chore: run test

--- examples/guard/src/pages/Login.tsx
+++ examples/guard/src/pages/Login.tsx
@@ -50,14 +50,23 @@ export default function Login () {
     guard.changeLang(lang)
   }, [lang])
 
-  const changeContentCSS = () => guard.changeContentCSS('body {background: red}')
+  const getUserInfo = async() => {
+    const userInfo = await guard.trackSession()
+    console.log('userInfo: ', userInfo)
+  }
+
+  const checkLoginStatus = async () => {
+    const user = await guard.checkLoginStatus()
+    console.log('user: ', user)
+  }
 
   return <>
     <div style={{ marginBottom: '100px' }}>
       <button onClick={onmountGuard}>unmount Guard Component</button>
       <button onClick={startRegister}>startRegister</button>
       <button onClick={changeLang}>changeLang</button>
-      <button onClick={changeContentCSS}>changeContentCSS</button>
+      <button onClick={getUserInfo}>getUserInfo</button>
+      <button onClick={checkLoginStatus}>checkLoginStatus</button>
     </div>
     
     <div id="guard-container"></div>
