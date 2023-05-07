44eaf761cc99e8d98605f66b94f28a5013ee8fff
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2023-05-07 16:26:38 +0800

    test: run examples for version 5.1.9-alpha.0

--- examples/guard-react/normal/src/App.tsx
+++ examples/guard-react/normal/src/App.tsx
@@ -13,10 +13,10 @@ import './App.css'
 export default function App() {
   return (
     <GuardProvider
-      appId="AUTHING_APP_ID"
+      appId="6319f41de72e9e18a1bafba6"
       
       // 如果你使用的是私有化部署的 Authing 服务，需要传入自定义 host，如:
-      // host="https://my-authing-app.example.com"
+      host="https://aaaaaa1bbv.authing.cn"
 
       // 默认情况下，会使用你在 Authing 控制台中配置的第一个回调地址为此次认证使用的回调地址。
       // 如果你配置了多个回调地址，也可以手动指定（此地址也需要加入到应用的「登录回调 URL」中）：
