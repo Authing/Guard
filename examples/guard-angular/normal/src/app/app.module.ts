108b04aca412b778f3e31eb81de5b20b25958aee
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2023-05-13 21:59:56 +0800

    test: run examples for version 5.1.10 :rocket:

--- examples/guard-angular/normal/src/app/app.module.ts
+++ examples/guard-angular/normal/src/app/app.module.ts
@@ -15,14 +15,14 @@ import * as facePlugin from 'face-api.js'
     BrowserModule,
     AppRoutingModule,
     GuardModule.forRoot({
-      appId: '6319f41de72e9e18a1bafba6',
+      appId: 'AUTHING_APP_ID',
 
       // 如果你使用的是私有化部署的 Authing 服务，需要传入自定义 host，如:
       // host: 'https://my-authing-app.example.com',
 
       // 默认情况下，会使用你在 Authing 控制台中配置的第一个回调地址为此次认证使用的回调地址。
       // 如果你配置了多个回调地址，也可以手动指定（此地址也需要加入到应用的「登录回调 URL」中）：
-      redirectUri: "https://aaaaaa1bbv.authing.cn",
+      // redirectUri: "https://aaaaaa1bbv.authing.cn",
 
       isSSO: true,
       config: {
