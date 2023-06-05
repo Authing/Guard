9c0b63bf6ab3c5894ba53105948aea7ad35d69e6
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2023-06-05 19:53:16 +0800

    test: run examples for version 6.0.0-alpha.15

--- examples/guard-react/normal/src/App.tsx
+++ examples/guard-react/normal/src/App.tsx
@@ -20,7 +20,7 @@ export default function App() {
 
       // 默认情况下，会使用你在 Authing 控制台中配置的第一个回调地址为此次认证使用的回调地址。
       // 如果你配置了多个回调地址，也可以手动指定（此地址也需要加入到应用的「登录回调 URL」中）：
-      // redirectUri="YOUR_REDIRECT_URI"
+      redirectUri="http://localhost:3003/callback"
 
       isSSO={true}
       facePlugin={facePlugin}
