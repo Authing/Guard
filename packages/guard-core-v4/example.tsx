3ec48881ed17a357ec156cd1996ed0c26a8be0c8
zhaojingjing <zhaojingjing@authing.com>
2024-01-17 10:44:17 +0800

    feat: eslint

--- packages/guard-core-v4/example.tsx
+++ packages/guard-core-v4/example.tsx
@@ -6,6 +6,7 @@ import * as facePlugin from 'face-api.js'
 
 const App = () => {
   const appId = '658e7ba8596b462d99912872'
+
   // const deviceId = '6486be0f60e50cb92678a468' //如要使用必须使用开启元数据对的 用户池 测试
 
   const config: Partial<GuardLocalConfig> = {
