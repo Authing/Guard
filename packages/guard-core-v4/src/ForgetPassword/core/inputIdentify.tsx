1b005711b52df8a5b552141d5845969e9c466d02
zhaojingjing <zhaojingjing@authing.com>
2024-06-24 15:00:05 +0800

    feat(forget-password): 重置密码删除手机号相关

--- packages/guard-core-v4/src/ForgetPassword/core/inputIdentify.tsx
+++ packages/guard-core-v4/src/ForgetPassword/core/inputIdentify.tsx
@@ -25,26 +25,25 @@ export const InputIdentify: React.FC<InputIdentifyProps> = props => {
 
   const { t } = useTranslation()
 
-  const verifyCodeMethodsText = useMemo<
-    Record<
-      VerifyLoginMethods,
-      {
-        t: string
-        sort: number
-      }
-    >
-  >(
+  const verifyCodeMethodsText = useMemo<any>(
+    // Record<
+    //   VerifyLoginMethods,
+    //   {
+    //     t: string
+    //     sort: number
+    //   }
+    // >
     () => ({
       'email-code': {
         t: t('common.email'),
         sort: 2
-      },
-      'phone-code': {
-        t: publicConfig?.internationalSmsConfig?.enabled
-          ? t('common.areaCodePhone')
-          : t('common.phoneNumber'),
-        sort: 1
       }
+      // 'phone-code': {
+      //   t: publicConfig?.internationalSmsConfig?.enabled
+      //     ? t('common.areaCodePhone')
+      //     : t('common.phoneNumber'),
+      //   sort: 1
+      // }
     }),
     [publicConfig, t]
   )
@@ -54,6 +53,7 @@ export const InputIdentify: React.FC<InputIdentifyProps> = props => {
       return t('login.inputAccount', {
         text: methods
           ?.map(item => verifyCodeMethodsText[item])
+          ?.filter(Boolean)
           .sort((a, b) => a.sort - b.sort)
           .map(item => item.t)
           .map((item, index) => (index === 0 ? `「${item}」` : item))
@@ -63,6 +63,7 @@ export const InputIdentify: React.FC<InputIdentifyProps> = props => {
       return t('login.inputAccount', {
         text: methods
           ?.map(item => verifyCodeMethodsText[item])
+          ?.filter(Boolean)
           .sort((a, b) => a.sort - b.sort)
           .map(item => item.t)
           .join(' / ')
