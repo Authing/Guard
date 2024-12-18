405a61f7a66b37c6e3688dc9e5f3f819a7f2ca3a
Zoyafng <meiqiuu@gmail.com>
2024-12-18 19:18:24 +0800

    build:remove example.tsx

--- packages/guard-core-v4/example.tsx
+++ packages/guard-core-v4/example.tsx
@@ -2,15 +2,13 @@ import { Guard, User, GuardLocalConfig } from './src/index'
 
 import { React, render } from 'shim-react'
 
-import * as facePlugin from 'face-api.js'
+// import * as facePlugin from 'face-api.js'
 
 const App = () => {
   const appId = 'AUTHING_APP_ID'
   // const deviceId = '6486be0f60e50cb92678a468' //如要使用必须使用开启元数据对的 用户池 测试
 
-  const config: Partial<GuardLocalConfig> = {
-
-  }
+  const config: Partial<GuardLocalConfig> = {}
 
   const onLogin = (userInfo: User) => {
     console.log(userInfo)
@@ -22,11 +20,11 @@ const App = () => {
       appId={appId}
       onLogin={onLogin}
       onLoginError={error => {
-        console.log(error,"loginerror")
+        console.log(error, 'loginerror')
       }}
       visible={true}
       config={config}
-      facePlugin={facePlugin}
+      // facePlugin={facePlugin}
     />
   )
 }
