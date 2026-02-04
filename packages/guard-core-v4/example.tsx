aef4467506abb242a0423827eaf4d988267045d7
Zoyafng <meiqiuu@gmail.com>
2026-02-04 20:02:51 +0800

    format: lint

--- packages/guard-core-v4/example.tsx
+++ packages/guard-core-v4/example.tsx
@@ -8,9 +8,7 @@ const App = () => {
   const appId = 'AUTHING_APP_ID'
   // const deviceId = '6486be0f60e50cb92678a468' //如要使用必须使用开启元数据对的 用户池 测试
 
-  const config: Partial<GuardLocalConfig> = {
-
-  }
+  const config: Partial<GuardLocalConfig> = {}
 
   const onLogin = (userInfo: User) => {
     console.log(userInfo)
@@ -18,11 +16,10 @@ const App = () => {
 
   return (
     <Guard
-      // deviceId={deviceId}
       appId={appId}
       onLogin={onLogin}
       onLoginError={error => {
-        console.log(error,"loginerror")
+        console.log(error, 'loginerror')
       }}
       visible={true}
       config={config}
