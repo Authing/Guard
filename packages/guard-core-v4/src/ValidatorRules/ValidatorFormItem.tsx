c2b676db02b79a1c1c208d0c2179087b90478ef7
zhaojingjing <zhaojingjing@authing.com>
2026-02-04 17:43:26 +0800

    feat: 屏蔽接口

--- packages/guard-core-v4/src/ValidatorRules/ValidatorFormItem.tsx
+++ packages/guard-core-v4/src/ValidatorRules/ValidatorFormItem.tsx
@@ -176,21 +176,18 @@ const ValidatorFormItem: React.FC<ValidatorFormItemMetaProps> = props => {
     rules.push(formatRules)
 
     // 是否校验重复
-    if (checkRepeat || checkExist) {
-      rules.push({
-        validator: checkRepeatFn,
-        validateTrigger: []
-      })
-    }
+    // if (checkRepeat || checkExist) {
+    //   rules.push({
+    //     validator: checkRepeatFn,
+    //     validateTrigger: []
+    //   })
+    // }
 
     return rules
   }, [
     required,
     methodContent.field,
-    formatRules,
-    checkRepeat,
-    checkExist,
-    checkRepeatFn
+    formatRules
   ])
   return (
     <Form.Item
