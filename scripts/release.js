78dc7db0d0e4204cc981dc7624536b32b1074d06
dongfengtao <dongfengtao@authing.com>
2026-04-20 18:17:18 +0800

    fix:酷锐程 打包配置文件

--- scripts/release.js
+++ scripts/release.js
@@ -52,6 +52,7 @@ function callShell(args) {
     ),
     `cd packages/guard-shim-react && ${releaseType}`,
     `cd packages/guard-shim-react18 && ${releaseType}`,
+    `sleep 15`, // 等待 npm registry 同步
     `cd packages/native-js-ui-components && npm ci && npm install --save-exact @authing/guard-shim-react@${version} && npm run build:lib && ${releaseType}`,
     `cd packages/react-ui-components && npm ci && npm install --save-exact @authing/guard-shim-react@${version} && npm run build:lib && ${releaseType}`,
     `cd packages/react18-ui-components && npm ci && npm install --save-exact @authing/guard-shim-react18@${version} && npm run build:lib && ${releaseType}`,
